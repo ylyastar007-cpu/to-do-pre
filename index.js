b3c26ea525b67cdd255f07d1d010e7ae375caf64
Ylyana <ylyastar007@gmail.com>
2025-12-07 14:48:55 +0300

    changejs

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 function loadTasks() {
-
+	const savedItems = localStorage.getItem('to-do');
+	if (savedItems) {
+		return JSON.parse(savedItems);
+	}
+	return items;
 }
 
 function createItem(item) {
@@ -22,14 +26,55 @@ function createItem(item) {
   const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
   const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
   const editButton = clone.querySelector(".to-do__item-button_type_edit");
-
+  	textElement.textContent = item;
+	deleteButton.addEventListener('click', function() {
+		clone.remove();
+		items = getTasksFromDOM();
+		saveTasks(items);
+	});
+	duplicateButton.addEventListener('click', function() {
+		const itemName = textElement.textContent ;
+		const newItem = createItem(item);
+		listElement.prepend(newItem);
+		items = getTasksFromDOM();
+		saveTasks(items);
+	});
+	editButton.addEventListener('click', function() {
+		textElement.setAttribute('contenteditable', 'true');
+		textElement.focus();
+	});
+	textElement.addEventListener('blur', function() {
+		textElement.removeAttribute('contenteditable');
+		items = getTasksFromDOM();
+		saveTasks(items);
+	});
+	return clone;
 }
+
+items = loadTasks();
+items.forEach(item => listElement.append(createItem(item)));
+
+formElement.addEventListener('submit', function(evnt) {
+	evnt.preventDefault();
+	const item = inputElement.value.trim();
+	if (item) {
+		const taskElements = createItem(item);
+		listElement.prepend(taskElements);
+		items = getTasksFromDOM();
+		saveTasks(items);
+		inputElement.value = '';
+	};
+});
 
 function getTasksFromDOM() {
-
-}
+	const itemsNamesElements = document.querySelectorAll('.to-do__item-text');
+	let tasks = [];
+	itemsNamesElements.forEach(function(element) {
+		tasks.push(element.textContent);
+	});
+	return tasks;
+};
 
 function saveTasks(tasks) {
-
+	localStorage.setItem('to-do', JSON.stringify(tasks));	
 }
-
